f76458df9672ea318d4aed427e6596ac36de53d8
Christina Sosa <csosa2106@gmail.com>
2016-07-16 11:19:28 -0600

    added user story and persona content to index.php

--- index.php
+++ index.php
@@ -2,16 +2,30 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<title>Netflix Data Design</title>
+		<title>Netflix Data Design Project</title>
 	</head>
 	<body>
 		<header>
 			<h1>Netflix Data Design</h1>
 		</header>
 		<main>
+			<h2>User Story</h2>
 			<p>
-				As a user, I want to watch films and television shows
+				As a user, I want to watch films and television shows.
 			</p>
+			<h2>Persona</h2>
+			<p>As a lover of TV and movies as well as a hater of Comcast Satellite television, Kendall wants comment,
+				review and do research on movies she has previously watched and will watch in the future. Kendall loves
+				watching horror movies and is very knowledgable in the genre. She would like to find more great horror
+				movies as well as contribute to the review section in Netflix to recommend and critique horror movies for
+				other Netflix users. Kendall uses her Netflix primarily on her streaming device (Amazon Firestick) and
+				her Macbook Air laptop. She does not use other devices to access Netflix although it is available through
+				mobile devices if ever needed.
+				Katy
+			<h3>Use Case 1</h3>
+
+			<h3>Use Case 2</h3>
+			<h2>Interaction Flow</h2>
 		</main>
 	</body>
 	<footer>
