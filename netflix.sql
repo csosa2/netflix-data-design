722494ec5e25bcf17b515ecef233c0c098f23763
Christina Sosa <csosa2106@gmail.com>
2016-07-23 20:05:47 -0600

    created media, review and helpful table

--- netflix.sql
+++ netflix.sql
@@ -4,8 +4,29 @@ DROP TABLE IF EXISTS review;
 DROP TABLE IF EXISTS helpful;
 
 CREATE TABLE user (
-	userId,
-	userName,
-	userPassHash,
-	userPassSalt,
+	userPassSalt CHAR(64),
+	userPassHash CHAR(128),
+	UNIQUE (userName),
+	PRIMARY KEY (userId)
+
+);
+
+CREATE TABLE media (
+	mediaFilePath VARCHAR(100) NOT NULL , /*How do I populate file paths int his column?*/
+	mediaTitle VARCHAR(200) NOT NULL,
+	PRIMARY KEY (mediaId)
+);
+
+CREATE TABLE review (
+	reviewContent VARCHAR(1999) NOT NULL, /*charchar min is 80. How do I insert this?*/
+	FOREIGN KEY (reviewUserId) REFERENCES user(userId),
+	FOREIGN KEY (reviewMediaId) REFERENCES media(mediaId),
+	PRIMARY KEY (reviewId)
+);
+
+CREATE TABLE helpful (
+	helpfulVote BOOLEAN,
+	helpfulInappropriate BOOLEAN,
+	FOREIGN KEY (helpfulUserId) REFERENCES user(userId),
+	FOREIGN KEY (helpfulReviewId) REFERENCES review(reviewId)
 );
